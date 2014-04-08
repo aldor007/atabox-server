943e7dcaaaa39dde2c62bf6d366e47328060730c
jaworekmichal <jaworekmichal@gmail.com>
2014-04-09 00:20:47 +0200

    Przykładowy main zawierający kod w C++11 (wątki)

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,30 @@
  *      Author: mj
  */
 
-int main (int argc, char** argv){
-	return 0;
+#include <iostream>
+#include <thread>
+
+static const int num_threads = 3;
+
+
+void call_from_thread(int tid) {
+    std::cout << "Wątek w stylu C++11 " << tid << std::endl;
+}
+
+int main() {
+    std::thread t[num_threads];
+
+    for (int i = 0; i < num_threads; ++i) {
+        t[i] = std::thread(call_from_thread, i);
+    }
+
+    std::cout << "To jest tylko test czy kompilacja w standardzie c++11 przebiega poprawnie\n";
+
+    for (int i = 0; i < num_threads; ++i) {
+        t[i].join();
+    }
+
+    return 0;
 }
 
 
