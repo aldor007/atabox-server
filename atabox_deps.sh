bce9050bb583865151203a3af06b52b11921e3a5
Marcin Kaciuba <aldor@Aldor.lan>
2014-04-08 21:57:29 +0200

    makefile work

---
+++ atabox_deps.sh
@@ -0,0 +1,7 @@
+sudo apt-get install zlib1g-dev -y
+sudo apt-get install zlib -y
+sudo apt-get install libbzip2-dev -y
+sudo apt-get install libsnappy-dev -y
+sudo apt-get install libgflags2 -y
+sudo apt-get isntall libgtest-dev -y
+
