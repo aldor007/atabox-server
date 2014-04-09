6c8bf22392b247b6140af0f7341e5e09748987e9
jaworekmichal <jaworekmichal@gmail.com>
2014-04-09 19:54:43 +0200

    Translation of main funtion.

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,7 @@ static const int num_threads = 3;
 
 
 void call_from_thread(int tid) {
-    std::cout << "Wątek w stylu C++11 " << tid << std::endl;
+    std::cout << "Thread in C++11 style" << tid << std::endl;
 }
 
 int main() {
@@ -22,7 +22,7 @@ int main() {
         t[i] = std::thread(call_from_thread, i);
     }
 
-    std::cout << "To jest tylko test czy kompilacja w standardzie c++11 przebiega poprawnie\n";
+    std::cout << "This funtions is used do determine if c++11 compilation is correct.\n";
 
     for (int i = 0; i < num_threads; ++i) {
         t[i].join();
