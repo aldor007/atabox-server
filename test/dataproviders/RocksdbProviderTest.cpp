a40955f162745e22535bc7b8dc26cf22d8a2e02a
Aldor007 <aldor@AldorNote.lan>
2014-04-09 22:32:27 +0200

    working on rocksdb

---
+++ test/dataproviders/RocksdbProviderTest.cpp
@@ -0,0 +1,18 @@
+/*
+ * RocksdbProviderTest.cpp
+ *
+ *  Created on: 9 kwi 2014
+ *      Author: aldor
+ */
+
+#include "RocksdbProviderTest.h"
+
+RocksdbProviderTest::RocksdbProviderTest() {
+	// TODO Auto-generated constructor stub
+
+}
+
+RocksdbProviderTest::~RocksdbProviderTest() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ test/dataproviders/RocksdbProviderTest.h
@@ -0,0 +1,17 @@
+/*
+ * RocksdbProviderTest.h
+ *
+ *  Created on: 9 kwi 2014
+ *      Author: aldor
+ */
+
+#ifndef ROCKSDBPROVIDERTEST_H_
+#define ROCKSDBPROVIDERTEST_H_
+
+class RocksdbProviderTest {
+public:
+	RocksdbProviderTest();
+	virtual ~RocksdbProviderTest();
+};
+
+#endif /* ROCKSDBPROVIDERTEST_H_ */
