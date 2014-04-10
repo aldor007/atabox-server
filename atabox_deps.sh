b6ae12318e105fa0047ca99b97c47c033908d109
Marcin Kaciuba <aldor007@gmail.com>
2014-04-10 20:00:16 +0200

    remove tests deps [how install in wiki]

--- atabox_deps.sh
+++ atabox_deps.sh
@@ -7,8 +7,8 @@ sudo apt-get install libgflags2 -y
 sudo apt-get install libgflags2-dev -y
 
 #test
-sudo apt-get install libgtest-dev -y
-sudo apt-get install google-mock -y 
+# sudo apt-get install libgtest-dev -y
+# sudo apt-get install google-mock -y 
 #coverage
 sudo apt-get install python-dev python-pip -y
 sudo apt-get install lcov -y
