31d072cc5db8f00aa3a7dab9e7635e4f26a0a0d3
jaworekmichal <jaworekmichal@gmail.com>
2014-05-01 11:02:52 +0200

    names is WaveFileAnalizatorTest corrected.

--- test/wave/WaveFileAnalizatorTest.cpp
+++ test/wave/WaveFileAnalizatorTest.cpp
@@ -12,16 +12,16 @@ class WaveFileAnalizatorTest: public ::testing::Test {
 
 TEST_F(WaveFileAnalizatorTest, findsMaximumCorrectly) {
 	//given
-	NormalizedSampleListMock waveFile;
-	ON_CALL(waveFile, getNumberOfSamples()).WillByDefault(Return(4));
-	ON_CALL(waveFile, getSample(0)).WillByDefault(Return(0.128));
-	ON_CALL(waveFile, getSample(1)).WillByDefault(Return(-0.5));
-	ON_CALL(waveFile, getSample(2)).WillByDefault(Return(0.16));
-	ON_CALL(waveFile, getSample(3)).WillByDefault(Return(0.130));
+	NormalizedSampleListMock samples;
+	ON_CALL(samples, getNumberOfSamples()).WillByDefault(Return(4));
+	ON_CALL(samples, getSample(0)).WillByDefault(Return(0.128));
+	ON_CALL(samples, getSample(1)).WillByDefault(Return(-0.5));
+	ON_CALL(samples, getSample(2)).WillByDefault(Return(0.16));
+	ON_CALL(samples, getSample(3)).WillByDefault(Return(0.130));
 
 	//when
 	WaveFileAnalizator analizator;
-	double amplitude = analizator.findAmplitude(waveFile);
+	double amplitude = analizator.findAmplitude(samples);
 
 	//then
 	ASSERT_DOUBLE_EQ(amplitude, 0.5);
@@ -29,17 +29,17 @@ TEST_F(WaveFileAnalizatorTest, findsMaximumCorrectly) {
 
 TEST_F(WaveFileAnalizatorTest, countsZeroCrossingsCorrectly) {
 	//given
-	NormalizedSampleListMock waveFile;
-	ON_CALL(waveFile, getNumberOfSamples()).WillByDefault(Return(5));
-	ON_CALL(waveFile, getSample(0)).WillByDefault(Return(-0.12));
-	ON_CALL(waveFile, getSample(1)).WillByDefault(Return(-0.130));
-	ON_CALL(waveFile, getSample(2)).WillByDefault(Return(0.148));
-	ON_CALL(waveFile, getSample(3)).WillByDefault(Return(0.130));
-	ON_CALL(waveFile, getSample(4)).WillByDefault(Return(-0.1));
+	NormalizedSampleListMock samples;
+	ON_CALL(samples, getNumberOfSamples()).WillByDefault(Return(5));
+	ON_CALL(samples, getSample(0)).WillByDefault(Return(-0.12));
+	ON_CALL(samples, getSample(1)).WillByDefault(Return(-0.130));
+	ON_CALL(samples, getSample(2)).WillByDefault(Return(0.148));
+	ON_CALL(samples, getSample(3)).WillByDefault(Return(0.130));
+	ON_CALL(samples, getSample(4)).WillByDefault(Return(-0.1));
 
 	//when
 	WaveFileAnalizator analizator;
-	int zeroCrossings = analizator.countZeroCrossings(waveFile);
+	int zeroCrossings = analizator.countZeroCrossings(samples);
 
 	//then
 	ASSERT_EQ(zeroCrossings, 2);
@@ -47,17 +47,17 @@ TEST_F(WaveFileAnalizatorTest, countsZeroCrossingsCorrectly) {
 
 TEST_F(WaveFileAnalizatorTest, countsPercentageAboveCorrectly) {
 	//given
-	NormalizedSampleListMock waveFile;
-	ON_CALL(waveFile, getNumberOfSamples()).WillByDefault(Return(5));
-	ON_CALL(waveFile, getSample(0)).WillByDefault(Return(-0.11));
-	ON_CALL(waveFile, getSample(1)).WillByDefault(Return(0.7));
-	ON_CALL(waveFile, getSample(2)).WillByDefault(Return(0.2));
-	ON_CALL(waveFile, getSample(3)).WillByDefault(Return(1));
-	ON_CALL(waveFile, getSample(4)).WillByDefault(Return(-1));
+	NormalizedSampleListMock samples;
+	ON_CALL(samples, getNumberOfSamples()).WillByDefault(Return(5));
+	ON_CALL(samples, getSample(0)).WillByDefault(Return(-0.11));
+	ON_CALL(samples, getSample(1)).WillByDefault(Return(0.7));
+	ON_CALL(samples, getSample(2)).WillByDefault(Return(0.2));
+	ON_CALL(samples, getSample(3)).WillByDefault(Return(1));
+	ON_CALL(samples, getSample(4)).WillByDefault(Return(-1));
 
 	//when
 	WaveFileAnalizator analizator;
-	double percentageAbove = analizator.percentageAbove(waveFile, 0.5);
+	double percentageAbove = analizator.percentageAbove(samples, 0.5);
 
 	//then
 	ASSERT_DOUBLE_EQ(percentageAbove, 3.0/5.0);
@@ -65,17 +65,17 @@ TEST_F(WaveFileAnalizatorTest, countsPercentageAboveCorrectly) {
 
 TEST_F(WaveFileAnalizatorTest, countsPercentageBelowCorrectly) {
 	//given
-	NormalizedSampleListMock waveFile;
-	ON_CALL(waveFile, getNumberOfSamples()).WillByDefault(Return(5));
-	ON_CALL(waveFile, getSample(0)).WillByDefault(Return(-0.11));
-	ON_CALL(waveFile, getSample(1)).WillByDefault(Return(0.7));
-	ON_CALL(waveFile, getSample(2)).WillByDefault(Return(0.2));
-	ON_CALL(waveFile, getSample(3)).WillByDefault(Return(1));
-	ON_CALL(waveFile, getSample(4)).WillByDefault(Return(-1));
+	NormalizedSampleListMock samples;
+	ON_CALL(samples, getNumberOfSamples()).WillByDefault(Return(5));
+	ON_CALL(samples, getSample(0)).WillByDefault(Return(-0.11));
+	ON_CALL(samples, getSample(1)).WillByDefault(Return(0.7));
+	ON_CALL(samples, getSample(2)).WillByDefault(Return(0.2));
+	ON_CALL(samples, getSample(3)).WillByDefault(Return(1));
+	ON_CALL(samples, getSample(4)).WillByDefault(Return(-1));
 
 	//when
 	WaveFileAnalizator analizator;
-	double percentageBelow = analizator.percetnageBelow(waveFile, 0.5);
+	double percentageBelow = analizator.percetnageBelow(samples, 0.5);
 
 	//then
 	ASSERT_DOUBLE_EQ(percentageBelow, 2.0/5.0);
