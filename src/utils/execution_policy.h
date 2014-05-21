bf95ab3f38bf9a62d94dc47fb23cc2b0e4afe70b
Marcin Kaciuba <aldor007@gmail.com>
2014-05-22 00:10:02 +0200

    add messing files

---
+++ src/utils/execution_policy.h
@@ -0,0 +1,33 @@
+/*
+#include <map>
+#include <string>
+#include <wave/WaveProperties.h>
+#include <recognition/PropertiesComparator.h>
+*/
+
+typedef std::map<WaveProperties, std::string>::iterator map_it;
+
+std::string execution_policy_strict(std::map<WaveProperties, std::string> &list, WaveProperties &waveProperties) {
+	PropertiesComparator comparator;
+	for (map_it iterator = list.begin(); iterator != list.end(); iterator++) {
+		double distance =  comparator.getDistance(iterator->first, waveProperties);
+		BOOST_LOG_TRIVIAL(debug)<<" Distance "<<distance;
+		if (fabs(distance - 0) < 0.000001) { //FIXME: get compersion precision from config file
+			return iterator->second;
+		}
+	}
+	return "";
+}
+std::string execution_policy_nonstrict(std::map<WaveProperties, std::string> &list, WaveProperties &waveProperties) {
+	PropertiesComparator comparator;
+	double minDistance = 10000;
+	std::string currentCommand;
+	for (map_it iterator = list.begin(); iterator != list.end(); iterator++) {
+		double distance =  comparator.getDistance(iterator->first, waveProperties);
+		BOOST_LOG_TRIVIAL(debug)<<" Distance "<<distance;
+		if (minDistance > distance) {
+			currentCommand = iterator->second;
+		}
+	}
+	return currentCommand;
+}
