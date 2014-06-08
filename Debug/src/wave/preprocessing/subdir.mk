4238b62b60073467cd6230040af90036ee4c2087
jaworekmichal <jaworekmichal@gmail.com>
2014-06-08 17:12:40 +0200

    conflict solving

--- Debug/src/wave/preprocessing/subdir.mk
+++ Debug/src/wave/preprocessing/subdir.mk
@@ -4,15 +4,12 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
-../src/wave/preprocessing/NormalizingFilter.cpp \
 ../src/wave/preprocessing/Preprocessor.cpp 
 
 OBJS += \
-./src/wave/preprocessing/NormalizingFilter.o \
 ./src/wave/preprocessing/Preprocessor.o 
 
 CPP_DEPS += \
-./src/wave/preprocessing/NormalizingFilter.d \
 ./src/wave/preprocessing/Preprocessor.d 
 
 
@@ -20,7 +17,7 @@ CPP_DEPS += \
 src/wave/preprocessing/%.o: ../src/wave/preprocessing/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -I../external/rocksdb_source/include -I../src -I../external/casablanca/Release/include -I../external/boost -I../external/kiss_fft -O0 -g3 -c -fmessage-length=0 -std=c++11  -DBOOST_LOG_DYN_LINK -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -std=c++0x -D__GXX_EXPERIMENTAL_CXX0X__ -I../external/rocksdb_source/include -I../src -I../external/casablanca/Release/include -I../external/boost -I../external/kiss_fft -O0 -g3 -c -fmessage-length=0 -std=c++11 -fprofile-arcs -ftest-coverage -DBOOST_LOG_DYN_LINK -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
