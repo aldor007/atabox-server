927154e39fe87e23436c042fac61faab4d83bcf4
Marcin Kaciuba <aldor007@gmail.com>
2015-03-05 23:33:52 +0100

    clean old dirs for building

--- Release/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lrocksdb -lboost_iostreams -lboost_system -lboost_filesystem -lpthread -lz -lcasablanca -lbz2 -lboost_log -lboost_thread -lboost_program_options
-
