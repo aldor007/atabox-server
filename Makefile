621d8ff1b64868bf8dc59fbe7583f71a7a67b8ef
Marcin Kaciuba <aldor007@gmail.com>
2015-03-06 09:03:18 +0100

    uncomment test

--- Makefile
+++ Makefile
@@ -26,7 +26,7 @@ rocksdb_lib:
 casablanca:
 	mkdir -p libs
 	mkdir -p $(EXTERNAL)/casablanca/Release/build.release
-	cd $(EXTERNAL)/casablanca/Release/build.release &&	cmake .. -DCMAKE_BUILD_TYPE=Release && 	make && sudo make install
+	cd $(EXTERNAL)/casablanca/Release/build.release &&	cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS=OFF -DBUILD_SAMLPES=OFF && 	make && sudo make install
 	cp $(EXTERNAL)/casablanca/Release/build.release/Binaries/libcpprest.so libs
 	sudo cp $(EXTERNAL)/casablanca/Release/build.release/Binaries/libcpprest.so /usr/lib
 aquila:
--- tests/dataproviders/RocksdbProviderTest.cpp
+++ tests/dataproviders/RocksdbProviderTest.cpp
@@ -11,48 +11,46 @@
 #undef U
 #include <string>
 #ifndef GTEST_H_
-	#define GTEST_H_
-	#include "gtest/gtest.h"
+    #define GTEST_H_
+    #include "gtest/gtest.h"
 #endif
 class RocksdbProviderTest: public ::testing::Test {
 
 };
 
-//
-// TEST_F(RocksdbProviderTest, writeDataCanItRead) {
-// 	std::string test = "test/file.db";
-//
-// 	RocksdbProvider<std::string, std::string> db(test);
-//
-// 	db.put("test", "value");
-// 	std::string tmp = db.get("test");
-// 	ASSERT_TRUE(tmp == "value");
-//
-// }
-//
-// TEST_F(RocksdbProviderTest, getListOfKeys) {
-// 	std::string test = "test/file.db";
-//
-// 	RocksdbProvider<std::string, std::string> db(test);
-//
-// 	db.put("test2", "value");
-// 	auto mapdata = db.getAllKV();
-//
-// 	ASSERT_TRUE(mapdata.size() >0);
-//
-// }
-//
-// TEST_F(RocksdbProviderTest, putAndGetWaveProperies) {
-// 	std::string dbname = "test/file.db";
-//
-// 	RocksdbProvider<jsonextend, std::string> db(dbname);
-// 	jsonextend test;
-// 	test["name"] = web::json::value::string("vim");
-// 	test["amplitude"] = web::json::value::number(0.2);
-// 	std::string expected = "vim /tmp/test";
-// 	db.put(test, expected);
-// 	ASSERT_EQ(db.get(test), expected);
-//
-//
-// }
-//
+
+TEST_F(RocksdbProviderTest, writeDataCanItRead) {
+    std::string test = "tests/file.db";
+
+    RocksdbProvider<std::string, std::string> db(test);
+
+    db.put("test", "value");
+    std::string tmp = db.get("test");
+    ASSERT_TRUE(tmp == "value");
+
+}
+
+TEST_F(RocksdbProviderTest, getListOfKeys) {
+    std::string test = "tests/file.db";
+
+    RocksdbProvider<std::string, std::string> db(test);
+
+    db.put("test2", "value");
+    auto mapdata = db.getAllKV();
+
+    ASSERT_TRUE(mapdata.size() >0);
+
+}
+
+TEST_F(RocksdbProviderTest, putAndGetWaveProperies) {
+    std::string dbname = "tests/file.db";
+
+    RocksdbProvider<jsonextend, std::string> db(dbname);
+    jsonextend test;
+    test["name"] = web::json::value::string("vim");
+    test["amplitude"] = web::json::value::number(0.2);
+    std::string expected = "vim /tmp/test";
+    db.put(test, expected);
+    ASSERT_EQ(db.get(test), expected);
+}
+
