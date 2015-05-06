58de48f92b67efcdf301a1c378bf6cd35225fbdc
Marcin Kaciuba <aldor007@gmail.com>
2015-05-06 14:20:53 +0200

    Start working on RBM

---
+++ src/recognition/network/RBM.h
@@ -0,0 +1,14 @@
+#ifndef RBM_H
+#define RBM_H
+class RBM
+{
+public:
+    RBM();
+    ~RBM();
+protected:
+    
+
+};
+
+
+#endif
---
+++ src/recognition/network/activation_functions.h
@@ -0,0 +1,16 @@
+##ifndef ACTIVATION_FUNCTION
+#define ACTIVATION_FUNCTION
+class activation_functions
+{
+public:
+    virutal activation_functions();
+    virutal ~activation_functions();
+    
+    
+};
+
+
+
+
+
+#endif
