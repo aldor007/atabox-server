337b9be8a317064d72cb19c8222c3929c1bcf0d1
Marcin Kaciuba <aldor007@gmail.com>
2015-10-04 20:20:54 +0200

    add test file, cmake change

---
+++ tests/recognition/network/RBMTest.cpp
@@ -0,0 +1,25 @@
+/*
+ * RVMTest.cpp
+ *
+ *  Created on: 04.10.2015
+ *      Author: aldor
+ */
+
+
+#include "recognition/network/RBM.h"
+
+#undef U
+#include <string>
+#ifndef GTEST_H_
+#define GTEST_H_
+#include "gtest/gtest.h"
+#endif
+class RBMTest: public ::testing::Test {
+
+};
+
+
+TEST_F(RBMTest, writeDataCanItRead) {
+
+}
+
